d903976be08f8fd2e2bdb549c7a6733de9cd877b
Sai Mahesh Babu <gipsya42@gmail.com>
2026-09-12 22:48:36 +0530

    Single Linked List😄

---
+++ singlelinkedlist/singlelinkedlist.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	Data int
+	Next *Node
+}
+
+func main() {
+	n1 := &Node{Data: 10}
+	n2 := &Node{Data: 20}
+	n3 := &Node{Data: 30}
+
+	n1.Next = n2
+	n2.Next = n3
+
+	current := n1
+
+	for current != nil {
+		fmt.Println(current.Data)
+		current = current.Next
+	}
+
+}
