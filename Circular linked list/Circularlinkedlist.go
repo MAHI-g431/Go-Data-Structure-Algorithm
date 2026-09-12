d21fcbabb3d311c32884f95480b4dd53413bade9
Sai Mahesh Babu <gipsya42@gmail.com>
2026-09-12 22:53:35 +0530

    ~Circular Linked List~

---
+++ Circular linked list/Circularlinkedlist.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	Data int
+	Next *Node
+}
+
+func main() {
+	n1 := &Node{Data: 10}
+	n2 := &Node{Data: 20}
+	n3 := &Node{Data: 30}
+	n4 := &Node{Data: 40}
+
+	n1.Next = n2
+	n2.Next = n3
+	n3.Next = n4
+	n4.Next = n1
+
+	current := n1
+
+	for current != nil {
+		fmt.Println(current.Data)
+		current = current.Next
+	}
+}
---
+++ Circular linked list/go.mod
@@ -0,0 +1,3 @@
+module circularlinkedlist
+
+go 1.26.4
